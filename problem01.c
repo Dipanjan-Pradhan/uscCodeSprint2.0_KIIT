405fc8e2db47b8bd12181c046898bf9a23712cea
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-06-14 02:08:49 +0530

    Can Shivam Buy Ice Cream?

---
+++ problem01.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_SIZE 100
+bool canBuyIceCream(int budget, int *iceCreamPrices, int count){
+    for (int i = 0; i < count; i++){
+        if (iceCreamPrices[i] == budget)
+            return true;
+    }
+    return false;
+}
+
+
+int main()
+{
+    int budget;
+    int *iceCreamPrices = NULL;
+    int count = 0;
+    char line[MAX_SIZE];
+
+    printf("budget = ");
+    fgets(line, sizeof(line), stdin);
+    sscanf(line, "%d", &budget);
+
+    printf("iceCreamPrices = ");
+    while (1)
+    {
+        if (fgets(line, sizeof(line), stdin) == NULL)
+            break;
+        if (strcmp(line, "\n") == 0)
+            break;
+
+        int price;
+        if (sscanf(line, "%d", &price) == 1)
+        {
+            int *temp = realloc(iceCreamPrices, (count + 1) * sizeof(int));
+            if (temp == NULL)
+            {
+                free(iceCreamPrices);
+                return 1;
+            }
+
+            iceCreamPrices = temp;
+            iceCreamPrices[count++] = price;
+        }
+    }
+
+        if (canBuyIceCream(budget, iceCreamPrices, count))
+        {
+            printf("true\n");
+        }
+        else
+        {
+            printf("false\n");
+        }
+
+        return 0;
+}
