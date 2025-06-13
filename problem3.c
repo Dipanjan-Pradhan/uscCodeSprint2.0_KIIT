9e896d7b1f9a54bec586c27a9bb6f87a8a7b1751
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-06-13 18:17:06 +0530

    day3: Magical days in the coding marathon

---
+++ problem3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void getVehicleNo(int n, int *vehicles)
+{
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", (vehicles + i));
+    }
+    return;
+}
+
+int checkJamHour(int n, int *vehicles)
+{
+    int count = 0;
+    for (int i = 1; i < n; i++)
+    {
+        if (*(vehicles + i) >= 50 && (*(vehicles + i) > *(vehicles + i - 1)))
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    int n;
+    printf("Enter the total no of hours: ");
+    scanf("%d", &n);
+
+    int *vehicles = (int *)malloc(n * sizeof(int));
+    if (vehicles == NULL)
+    {
+        printf("Memory allocation failed");
+        return 1;
+    }
+    printf("Enter the no of vehicles for each hour:");
+    getVehicleNo(n, vehicles);
+
+    int jamHour = checkJamHour(n, vehicles);
+
+    printf("Total Critical Jam Hour = %d", jamHour);
+    free(vehicles);
+    return 0;
+}
