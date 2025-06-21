e9695be27f0089aaf6f43570b4e4afdd3ad5ed0e
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-06-22 00:55:37 +0530

    The Magic Stack Mirror

--- problem33.c
+++ problem33.c
@@ -4,18 +4,15 @@
 
 #define MAX_LEN 10000
 
-// Stack structure
 typedef struct {
     char data[MAX_LEN];
     int top;
 } Stack;
 
-// Function to initialize the stack
 void initStack(Stack *stack) {
     stack->top = -1;
 }
 
-// Function to push a character onto the stack
 void push(Stack *stack, char ch) {
     if (stack->top < MAX_LEN - 1) {
         stack->data[++(stack->top)] = ch;
@@ -25,7 +22,6 @@ void push(Stack *stack, char ch) {
     }
 }
 
-// Function to pop a character from the stack
 char pop(Stack *stack) {
     if (stack->top >= 0) {
         return stack->data[(stack->top)--];
@@ -35,7 +31,6 @@ char pop(Stack *stack) {
     }
 }
 
-// Check if the stack is empty
 int isEmpty(Stack *stack) {
     return stack->top == -1;
 }
@@ -47,21 +42,17 @@ int main() {
 
     printf("🔤 Please enter your sentence below:\n> ");
 
-    // Read the full input line
     fgets(input, sizeof(input), stdin);
 
-    // Remove trailing newline if present
     size_t len = strlen(input);
     if (len > 0 && input[len - 1] == '\n') {
         input[len - 1] = '\0';
     }
 
-    // Push characters onto the stack
     for (int i = 0; input[i] != '\0'; i++) {
         push(&stack, input[i]);
     }
 
-    // Display reversed string
     printf("Reversed Sentence:\n> ");
     while (!isEmpty(&stack)) {
         printf("%c", pop(&stack));
