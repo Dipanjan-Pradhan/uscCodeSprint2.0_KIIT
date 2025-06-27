119254d8bc0a75b363a6bb4132f4fe965c78cc3a
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-06-28 00:49:06 +0530

    Trapping Rainwater

--- problem43.c
+++ problem43.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// This function calculates how much rainwater can be trapped
 int calculateTrappedWater(int *height, int n) {
     int left = 0, right = n - 1;
     int leftMax = 0, rightMax = 0;
@@ -34,20 +33,17 @@ int main() {
     printf("How many buildings are there? ");
     scanf("%d", &n);
 
-    // Basic input check
     if (n <= 0) {
         printf("Number of buildings must be greater than zero.\n");
         return 1;
     }
 
-    // Allocate space to store building heights
     int *heights = (int *)malloc(n * sizeof(int));
     if (heights == NULL) {
         printf("Failed to allocate memory.\n");
         return 1;
     }
 
-    // Ask user to enter the building heights
     printf("Enter the height of each building :\n");
     for (int i = 0; i < n; i++) {
         scanf("%d", &heights[i]);
@@ -59,7 +55,6 @@ int main() {
         }
     }
 
-    // Call the function and display the result
     int trapped = calculateTrappedWater(heights, n);
     printf("The total amount of trapped rainwater is: %d units\n", trapped);
 
