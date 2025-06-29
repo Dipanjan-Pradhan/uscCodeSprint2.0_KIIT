42d9f826851841a8fc1d7af40b14542a43a1078c
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-06-30 02:34:18 +0530

    Reversing The Queue Of Queueville

--- problem48.c
+++ problem48.c
@@ -4,10 +4,8 @@
 #define MAX_QUEUE_LEN 100000
 #define MAX_ID 1000000000
 
-void getCitizensNo(int *n)
-{
-    while (1)
-    {
+void getCitizensNo(int *n){
+    while (1){
         printf("Enter the no of citizens in the queue (1 - %d): ", MAX_QUEUE_LEN);
         scanf("%d", n);
         if (*n >= 1 && *n <= MAX_QUEUE_LEN)
@@ -16,54 +14,44 @@ void getCitizensNo(int *n)
     }
 }
 
-int isValidId(int n)
-{
+int isValidId(int n){
     if (n >= 0 && n <= MAX_ID)
         return 1;
     return 0;
 }
 
-void getQueueId(int *queue, int n)
-{
+void getQueueId(int *queue, int n){
     printf("Enter the ID's of the citizens (0 - %d): ", MAX_ID);
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++){
         scanf("%d", &queue[i]);
-        if (!isValidId(queue[i]))
-        {
+        if (!isValidId(queue[i])){
             printf("!! Invalid ID !!\n");
             i--;
-        };
+        }
     }
 }
 
-void reverseQueue(int *queue, int n)
-{
+void reverseQueue(int *queue, int n){
     int temp;
-    for (int i = 0; i < n / 2; i++)
-    {
+    for (int i = 0; i < n / 2; i++){
         temp = queue[i];
         queue[i] = queue[n - i - 1];
         queue[n - i - 1] = temp;
     }
 }
 
-void printQueue(int *queue, int n)
-{
+void printQueue(int *queue, int n){
     printf("Reversed Array: ");
     for (int i = 0; i < n; i++)
-    {
         printf("%d ", queue[i]);
-    }
+    
 }
 
-int main()
-{
+int main(){
     int n;
     getCitizensNo(&n);
     int *queue = (int *)malloc(n * sizeof(int));
-    if (!queue)
-    {
+    if (!queue){
         printf("Memory Allocation Failed\n");
         exit(1);
     }
