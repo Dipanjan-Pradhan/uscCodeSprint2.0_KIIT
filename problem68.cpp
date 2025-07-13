15bf37ca6d012fa53010380a157670f2e43b9668
Dipanjan-Pradhan <dipanjanpradhan686@gmail.com>
2025-07-13 21:31:53 +0530

    The Kingdom's Winter Heater Challenge

---
+++ problem68.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <functional>
+using namespace std;
+
+class WinterHeater {
+    vector<int> houses;
+    vector<int> heaters;
+
+public:
+    void readInput() {
+        string line;
+        int x;
+
+        // Read houses
+        cout << "houses = ";
+        getline(cin, line);
+        istringstream hs(line);
+        while (hs >> x)
+            houses.push_back(x);
+
+        // Read heaters
+        cout << "heaters = ";
+        getline(cin, line);
+        istringstream ht(line);
+        while (ht >> x)
+            heaters.push_back(x);
+    }
+
+    int findMinRadius() {
+        sort(houses.begin(), houses.end());
+        sort(heaters.begin(), heaters.end());
+
+        int radius = 0;
+
+        for (int house : houses) {
+            auto it = lower_bound(heaters.begin(), heaters.end(), house);
+
+            int dist1 = INT_MAX, dist2 = INT_MAX;
+
+            if (it != heaters.end()) dist1 = abs(*it - house);
+            if (it != heaters.begin()) dist2 = abs(*prev(it) - house);
+
+            radius = max(radius, min(dist1, dist2));
+        }
+
+        return radius;
+    }
+
+    void display() {
+        cout << findMinRadius() << "\n";
+    }
+};
+
+int main() {
+    WinterHeater wh;
+    wh.readInput();
+    wh.display();
+    return 0;
+}
